695071ba03b990e9654bc4a3de9208fa8624a092
Adam <adamwahlberg@hotmail.com>
2013-03-04 16:37:02 +0100

    Lagt till PianoViewController

---
+++ BeatLoops/BeatLoops/PianoViewController.h
@@ -0,0 +1,13 @@
+//
+//  PianoViewController.h
+//  BeatLoops
+//
+//  Created by admin on 3/4/13.
+//  Copyright (c) 2013 Adam Wilsby Wahlberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PianoViewController : UIViewController
+
+@end
---
+++ BeatLoops/BeatLoops/PianoViewController.m
@@ -0,0 +1,38 @@
+//
+//  PianoViewController.m
+//  BeatLoops
+//
+//  Created by admin on 3/4/13.
+//  Copyright (c) 2013 Adam Wilsby Wahlberg. All rights reserved.
+//
+
+#import "PianoViewController.h"
+
+@interface PianoViewController ()
+
+@end
+
+@implementation PianoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
