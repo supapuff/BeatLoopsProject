6172a094e65922235d0e6c25646f1e68398e8a19
Adam <adamwahlberg@hotmail.com>
2013-03-04 16:36:16 +0100

    Lagt till GuitarViewController

---
+++ BeatLoops/BeatLoops/GuitarViewController.m
@@ -0,0 +1,38 @@
+//
+//  GuitarViewController.m
+//  BeatLoops
+//
+//  Created by admin on 3/4/13.
+//  Copyright (c) 2013 Adam Wilsby Wahlberg. All rights reserved.
+//
+
+#import "GuitarViewController.h"
+
+@interface GuitarViewController ()
+
+@end
+
+@implementation GuitarViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
